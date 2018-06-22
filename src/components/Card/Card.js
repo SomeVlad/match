0b267467bb1bbe68963ac4498dd4c130ffb914c9
Vlad Samoylov <samoylov.dev@gmail.com>
2018-06-22 21:44:52 +0300

    eslint -> prettier

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,28 +1,26 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import "./Card.css"
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './Card.css'
 
 export class Card extends Component {
-  static defaultProps = {
-    front: ""
-  }
-  static propTypes = {
-    front: PropTypes.string,
-    back: PropTypes.string.isRequired
-  }
+    static defaultProps = {
+        front: ''
+    }
+    static propTypes = {
+        front: PropTypes.string,
+        back: PropTypes.string.isRequired
+    }
 
-  render() {
-    const { flipped, solved, errored } = this.props
-    const className = `card${flipped ? " flipped" : ""}${solved ? " solved" : ""}${
-      errored ? " errored" : ""
-    }`
-    return (
-      <div className={className} onClick={() => this.props.onClick()}>
-        <div className="front">{this.props.front}</div>
-        <div className="back">
-          <span>{this.props.back}</span>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        const { flipped, solved, errored, front, back, onClick } = this.props
+        const className = `card${flipped ? ' flipped' : ''}${solved ? ' solved' : ''}${errored ? ' errored' : ''}`
+        return (
+            <div className={className} onClick={onClick}>
+                <div className="front">{front}</div>
+                <div className="back">
+                    <span>{back}</span>
+                </div>
+            </div>
+        )
+    }
 }
--- src/components/ItemsStore/ItemsStore.js
+++ src/components/ItemsStore/ItemsStore.js
@@ -1,21 +1,21 @@
-import { food } from "../Emoji/Emoji"
+import { food } from '../Emoji/Emoji'
 
 export const Store = {
-  emojis: food.filter(Boolean),
-  getRandomIndex: maxValue => Math.floor(Math.random() * maxValue),
-  shuffle: array => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      ;[array[i], array[j]] = [array[j], array[i]]
+    emojis: food.filter(Boolean),
+    getRandomIndex: maxValue => Math.floor(Math.random() * maxValue),
+    shuffle: array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1))
+            ;[array[i], array[j]] = [array[j], array[i]]
+        }
+        return array
+    },
+    getArrayOfDoubles(length) {
+        const result = []
+        for (let i = 0; i < length; i += 2) {
+            const pickedValue = this.emojis.splice(this.getRandomIndex(this.emojis.length), 1)[0]
+            result.push(pickedValue, pickedValue)
+        }
+        return this.shuffle(result)
     }
-    return array
-  },
-  getArrayOfDoubles(length) {
-    const result = []
-    for (let i = 0; i < length; i += 2) {
-      const pickedValue = this.emojis.splice(this.getRandomIndex(this.emojis.length), 1)[0]
-      result.push(pickedValue, pickedValue)
-    }
-    return this.shuffle(result)
-  }
 }
